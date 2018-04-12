3f1ea20484a46eb348b45bf6e62c649425101805
Kevin Lozano <KEVINSILLO_15@hotmail.com>
2018-04-11 19:22:31 -0500

    german ya no es gay

--- WorldParkingVol1/Controllers/personasController.cs
+++ WorldParkingVol1/Controllers/personasController.cs
@@ -16,6 +16,7 @@ namespace WorldParkingVol1.Controllers
         private ParkingEntities1 db = new ParkingEntities1();
 
         // crud para procesos
+        // prueba commit
         
         // GET: personas
         public ActionResult Index()
